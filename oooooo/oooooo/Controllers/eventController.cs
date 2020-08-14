d0cd285323de69c1754de992b2def84db416493e
debbieccc <yitung532@hotmail.com>
2020-08-14 15:03:29 +0800

    新增活動頁面

---
+++ oooooo/oooooo/Controllers/eventController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace oooooo.Controllers
+{
+    public class eventController : Controller
+    {
+        dbecoDailyEntities db = new dbecoDailyEntities();
+        // GET: event
+        public ActionResult Event()
+        {
+            var table = from t in db.tEvent
+                        select t;
+            return View(table);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(tEvent p)
+        {
+            dbecoDailyEntities db = new dbecoDailyEntities();
+            db.tEvent.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Event");
+        }
+
+        public ActionResult SetRegistForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SetRegistForm(tEventRegister p)
+        {
+            dbecoDailyEntities db = new dbecoDailyEntities();
+            db.tEventRegister.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Event");
+        }
+    }
+}
