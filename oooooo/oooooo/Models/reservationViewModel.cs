d413618b23592459daaac2175f60f9f7bf4ca589
CGW1996 <s22760081@gmail.com>
2020-08-14 13:20:28 +0800

    financial reservation

---
+++ oooooo/oooooo/Models/reservationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace oooooo.Models
+{
+    public class reservationViewModel
+    {
+        public int fReservationId { get; set; }
+        public string fReservationType { get; set; }
+        public string fUserId { get; set; }
+        public Nullable<System.DateTime> fReservationDate { get; set; }
+        public string fReservationTime { get; set; }
+        public Nullable<int> fnumReservation { get; set; }
+        public string fRemark { get; set; }
+    }
+}
