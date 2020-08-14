e8ddfea229a6d83285dd6255a20feb600716c91a
debbieccc <yitung532@hotmail.com>
2020-08-14 10:33:08 +0800

    1234567

---
+++ oooooo/oooooo/Controllers/A1234567Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace oooooo.Controllers
+{
+    public class A1234567Controller : Controller
+    {
+        // GET: A1234567
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
