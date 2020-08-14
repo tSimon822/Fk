10d4f5d1efb0c0d51266d4f5b6cc5775a65ae9a0
debbieccc <yitung532@hotmail.com>
2020-08-14 13:52:15 +0800

    新增郵務管理

---
+++ oooooo/oooooo/Controllers/CargoController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace oooooo.Controllers
+{
+    public class CargoController : Controller
+    {
+        // GET: Cargo
+        public ActionResult Cargo()
+        {
+            dbecoDailyEntities db = new dbecoDailyEntities();
+            var package = from t in (new dbecoDailyEntities()).tCargo
+                          select t;
+            return View(package);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(tCargo p)
+        {
+            dbecoDailyEntities db = new dbecoDailyEntities();
+            db.tCargo.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Cargo");
+        }
+
+    }
+}
