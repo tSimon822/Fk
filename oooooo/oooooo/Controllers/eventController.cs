3a995e57b901fa9d12035a09d71f3fc045a97029
debbieccc <67948229+debbieccc@users.noreply.github.com>
2020-08-16 19:46:22 +0800

    edit

--- oooooo/oooooo/Controllers/eventController.cs
+++ oooooo/oooooo/Controllers/eventController.cs
@@ -44,5 +44,56 @@ namespace oooooo.Controllers
             db.SaveChanges();
             return RedirectToAction("Event_B");
         }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            return RedirectToAction("Event_B");
+
+            dbecoDailyEntities db = new dbecoDailyEntities();
+            tEvent x = db.tEvent.FirstOrDefault(m => m.fEventId == id);
+            return View(x);
+        }
+        
+            
+        [HttpPost]
+        public ActionResult Edit(tEvent p)
+        {
+            if (string.IsNullOrEmpty(p.fEventTitle))
+                return RedirectToAction("List");
+            dbecoDailyEntities db = new dbecoDailyEntities();
+            tEvent editevent = db.tEvent.FirstOrDefault(m => m.fEventId == p.fEventId);
+            if (editevent != null)
+            {
+                editevent.fEventTitle = p.fEventTitle;
+                editevent.fCategory = p.fCategory;
+                editevent.fEventFromDay = p.fEventFromDay;
+                editevent.fEventEndDate = p.fEventEndDate;
+                editevent.fUserId = p.fUserId;
+                editevent.fEventImgPath = p.fEventImgPath;
+                editevent.fEventDay = p.fEventDay;
+                editevent.fEventTime = p.fEventTime;
+                editevent.fEventLocation = p.fEventLocation;
+                editevent.fEventDescription = p.fEventDescription;
+                editevent.fEventFee = p.fEventFee;
+                editevent.fnumCourse = p.fnumCourse;
+                editevent.fRemark = p.fRemark;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Event_B");
+        }
+
+        public ActionResult Detail()
+        {
+            dbecoDailyEntities db = new dbecoDailyEntities();
+            var details = from t in (new dbecoDailyEntities()).tEventRegister
+                         select t;
+            return View(details);
+        }
+
+       
+
+
+
     }
 }
